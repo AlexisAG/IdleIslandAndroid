030d3f203b932850d74ad6446a0c34e3f4a3969e
AlexisAG <agay@amiltone.com>
2020-05-03 21:02:42 +0200

    Create gamemode + config scenes

---
+++ the_fantastic_island/Assets/TheFantasticIsland/GameMode/MainGameMode.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using AgToolkit.Core.GameModes;
+using AgToolkit.Core.Pool;
+using UnityEngine;
+
+public class MainGameMode : GameMode
+{
+    public override IEnumerator OnLoad()
+    {
+        return base.OnLoad();
+        //todo: pool
+        //todo: Load data 
+    }
+}
