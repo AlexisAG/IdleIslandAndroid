8e2817263a06d9aaef1eb27407ad4cc075daf950
AlexisAG <agay@amiltone.com>
2020-06-03 21:55:41 +0200

    UI Script

---
+++ the_fantastic_island/Assets/TheFantasticIsland/Scripts/UI/ScrollableInterface.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollableInterface : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _Contents = null;
+
+
+    private void Awake()
+    {
+        Debug.Assert(_Contents != null, $"{gameObject.name} Content is null.");
+    }
+
+    public void AddContent(GameObject obj)
+    {
+        obj.transform.SetParent(_Contents.transform);
+        obj.transform.SetAsLastSibling();
+    }
+
+    public void AddContent(GameObject obj, int place)
+    {
+        obj.transform.SetParent(_Contents.transform);
+        obj.transform.SetSiblingIndex(place);
+    }
+
+    public void RemoveContent(int index)
+    {
+        Transform obj = _Contents.transform.GetChild(index);
+
+        Debug.Assert(obj != null, $"There is no object in {gameObject.name} contents with index {index}.");
+        if (obj == null) return;
+
+        Destroy(obj);
+    }
+
+    public void RemoveContent(GameObject obj)
+    {
+        if (!obj.transform.IsChildOf(_Contents.transform)) return;
+
+        Destroy(obj);
+    }
+
+    public void ClearContents()
+    {
+        for (int i = 0; i < _Contents.transform.childCount; i++)
+        {
+            Destroy(_Contents.transform.GetChild(i).gameObject);
+        }
+    }
+}
