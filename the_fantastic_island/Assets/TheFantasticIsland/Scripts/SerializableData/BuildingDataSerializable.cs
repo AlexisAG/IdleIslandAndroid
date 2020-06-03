ceee332f9b525e2a865fe56e5a65171f1b1abed3
AlexisAG <agay@amiltone.com>
2020-06-03 20:44:12 +0200

    building data serializable

---
+++ the_fantastic_island/Assets/TheFantasticIsland/Scripts/SerializableData/BuildingDataSerializable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class BuildingDataSerializable : DataSerializable
+{
+    public string BuildingId { get; private set; }
+    public int SizeLevel { get; private set; }
+    public int ProductionLevel { get; private set; }
+
+    public BuildingDataSerializable(string id, int size, int prod)
+    {
+        BuildingId = id;
+        SizeLevel = size;
+        ProductionLevel = prod;
+    }
+}
