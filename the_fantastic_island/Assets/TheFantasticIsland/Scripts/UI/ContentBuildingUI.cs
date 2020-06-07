b453c3fea93245bcdf9014d0a9b2baa13ed9bf71
AlexisAG <agay@amiltone.com>
2020-06-07 13:09:25 +0200

    UI prefab & script

---
+++ the_fantastic_island/Assets/TheFantasticIsland/Scripts/UI/ContentBuildingUI.cs
@@ -0,0 +1,121 @@
+﻿using TheFantasticIsland.Helper;
+using TheFantasticIsland.Manager;
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+
+namespace TheFantasticIsland.Ui
+{
+    public class ContentBuildingUI : MonoBehaviour
+    {
+        [SerializeField]
+        private Image _Icon = null;
+        [SerializeField]
+        private Text _Title = null;
+        [SerializeField]
+        private Text _Info = null;
+        [SerializeField]
+        private Text _PorductionLevel = null;
+        [SerializeField]
+        private Text _SizeLevel = null;
+        [SerializeField]
+        private Button _Unlock = null;
+        [SerializeField]
+        private Button _IncreaseProduction = null;
+        [FormerlySerializedAs("_IncreaseLevel")] [SerializeField]
+        private Button _IncreaseSize = null;
+        [SerializeField]
+        private GameObject _ProductionChild = null;
+        [SerializeField]
+        private GameObject _SizeChild = null;
+
+        private float _ProductionAmount = 0f;
+        private float _SizeAmount = 0f;
+        private Resource _ProductionCostR = Resource.None;
+        private Resource _SizeCostR = Resource.None;
+
+        public Button Unlock => _Unlock;
+        public Button IncreaseProduction => _IncreaseProduction;
+        public Button IncreaseSize => _IncreaseSize;
+
+        private void Awake()
+        {
+            _ProductionChild.SetActive(false);
+            _SizeChild.SetActive(false);
+        }
+
+        private void LateUpdate()
+        {
+            if (_ProductionCostR == Resource.None || _SizeCostR == Resource.None) return;
+
+            bool unlockStatus = Unlock.enabled;
+            Unlock.enabled = ResourceManager.Instance.GetAmount(_ProductionCostR) >= _ProductionAmount;
+
+            if (unlockStatus != Unlock.enabled) {
+                Unlock.gameObject.GetComponent<Image>().color = Unlock.enabled ? Color.green : Color.gray;
+            }
+
+            if (!IncreaseProduction.gameObject.activeSelf || !IncreaseSize.gameObject.activeSelf) return;
+
+            bool prodStatus = IncreaseProduction.enabled;
+            bool sizeStatus = IncreaseSize.enabled;
+
+            IncreaseProduction.enabled = ResourceManager.Instance.GetAmount(_ProductionCostR) >= _ProductionAmount;
+            IncreaseSize.enabled = ResourceManager.Instance.GetAmount(_SizeCostR) >= _SizeAmount;
+
+            // Set Color Button
+            if (prodStatus != IncreaseProduction.enabled)
+            {
+                IncreaseProduction.gameObject.GetComponent<Image>().color = IncreaseProduction.enabled ? Color.green : Color.gray;
+            }
+            if (sizeStatus != IncreaseSize.enabled)
+            {
+                IncreaseSize.gameObject.GetComponent<Image>().color = IncreaseSize.enabled ? Color.green : Color.gray;
+            }
+
+        }
+
+        public void SetIcon(Sprite s)
+        {
+            _Icon.sprite = s;
+        }
+
+        public void SetName(string title)
+        {
+            _Title.text = title;
+        }
+
+        public void SetProductionInfo(string productionInfo)
+        {
+            _Info.text = productionInfo;
+        }
+
+        public void SetProductionLevel(int level)
+        {
+            _PorductionLevel.text = $"Current level: {level}";
+        }
+        public void SetSizeLevel(int level)
+        {
+            _SizeLevel.text = $"Current level: {level}";
+        }
+
+        public void ContentUnlocked()
+        {
+            _ProductionChild.SetActive(true);
+            _SizeChild.SetActive(true);
+            _Unlock.gameObject.SetActive(false);
+        }
+
+        public void SetAmount(float prod, float size)
+        {
+            _ProductionAmount = prod;
+            _SizeAmount = size;
+        }
+
+        public void SetCostResourceType(Resource prod, Resource size)
+        {
+            _ProductionCostR = prod;
+            _SizeCostR = size;
+        }
+    }
+}
--- the_fantastic_island/Assets/TheFantasticIsland/Scripts/UI/ScrollableInterface.cs
+++ the_fantastic_island/Assets/TheFantasticIsland/Scripts/UI/ScrollableInterface.cs
@@ -2,52 +2,63 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class ScrollableInterface : MonoBehaviour
+namespace TheFantasticIsland.Ui
 {
-    [SerializeField]
-    private GameObject _Contents = null;
-
-
-    private void Awake()
+    public class ScrollableInterface : MonoBehaviour
     {
-        Debug.Assert(_Contents != null, $"{gameObject.name} Content is null.");
-    }
+        [SerializeField]
+        private GameObject _Contents = null;
+        [SerializeField]
+        private GameObject _Title = null;
 
-    public void AddContent(GameObject obj)
-    {
-        obj.transform.SetParent(_Contents.transform);
-        obj.transform.SetAsLastSibling();
-    }
 
-    public void AddContent(GameObject obj, int place)
-    {
-        obj.transform.SetParent(_Contents.transform);
-        obj.transform.SetSiblingIndex(place);
-    }
-
-    public void RemoveContent(int index)
-    {
-        Transform obj = _Contents.transform.GetChild(index);
-
-        Debug.Assert(obj != null, $"There is no object in {gameObject.name} contents with index {index}.");
-        if (obj == null) return;
-
-        Destroy(obj);
-    }
-
-    public void RemoveContent(GameObject obj)
-    {
-        if (!obj.transform.IsChildOf(_Contents.transform)) return;
-
-        Destroy(obj);
-    }
-
-    public void ClearContents()
-    {
-        for (int i = 0; i < _Contents.transform.childCount; i++)
+        private void Awake()
         {
-            Destroy(_Contents.transform.GetChild(i).gameObject);
+            Debug.Assert(_Contents != null, $"{gameObject.name} Content is null.");
+        }
+
+        public void AddContent(GameObject obj)
+        {
+            obj.transform.SetParent(_Contents.transform);
+            obj.transform.SetAsLastSibling();
+        }
+
+        public void AddContent(GameObject obj, int place)
+        {
+            obj.transform.SetParent(_Contents.transform);
+            obj.transform.SetSiblingIndex(place);
+        }
+
+        public void RemoveContent(int index)
+        {
+            Transform obj = _Contents.transform.GetChild(index);
+
+            Debug.Assert(obj != null, $"There is no object in {gameObject.name} contents with index {index}.");
+            if (obj == null) return;
+
+            Destroy(obj);
+        }
+
+        public void RemoveContent(GameObject obj)
+        {
+            if (!obj.transform.IsChildOf(_Contents.transform)) return;
+
+            Destroy(obj);
+        }
+
+        public void ClearContents()
+        {
+            for (int i = 0; i < _Contents.transform.childCount; i++)
+            {
+                Destroy(_Contents.transform.GetChild(i).gameObject);
+            }
+        }
+
+        public void SetTitle(string title)
+        {
+            _Title.GetComponentInChildren<Text>().text = title;
         }
     }
 }
